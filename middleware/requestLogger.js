19f420c4d66e185241664fdc0c9c6aa006beee92
Stanton <smhermanus@gmail.com>
2024-11-21 02:07:45 +0200

    set

--- middleware/requestLogger.js
+++
@@ -1,5 +0,0 @@
-export function requestLogger(req, res, next) {
-    console.log(`${new Date().toISOString()} - ${req.method} ${req.path}`);
-    console.log('Request body:', req.body);
-    next();
-  }
